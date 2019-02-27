7fd1c7ecaad68ff74d16c6d1d97e11a3dc0c1044
xmzlwyq <47937513+xmzlwyq@users.noreply.github.com>
2019-02-27 16:26:49 +0800

    Create test.php

---
+++ mygroup/php/test.php
@@ -0,0 +1,12 @@
+<?php
+ini_set("display_errors", "on"); //开提示
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
+header("Access-Control-Allow-Origin:*"); //*号表示所有域名都可以访问
+header("Access-Control-Allow-Method:POST,GET");
+header("Content-type: text/html; charset=utf-8");
+
+if($_GET['code']==1)
+{
+    echo "ssss";
+}
+?>
